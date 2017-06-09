ed215a175e657dd12ecd4a567930c25089163f24
Peter Bloem <git@peterbloem.nl>
2017-06-09 15:31:34 +0200

    ...

---
+++ fractal-plr/img/runs/koch2/koch2.sh
@@ -0,0 +1,12 @@
+# shell for the job:
+#PBS -S /bin/bash
+# use one node with eight cores:
+#PBS -lnodes=1:cores16:cpu3:mem64gb
+#PBS -lwalltime=12:00:00
+module load java/1.8u40
+
+cd $HOME/experiments/fractals/runs/koch2
+
+# call the programs
+java -Xmx56g -jar $HOME/lilian/ifsem.jar --ifs.hq --type ifs --components 2 --repeats 8 --samples 500 --iterations 300 --data-source koch --data-size 50000 --with-init --num-candidates 500 > sys.log 2>&1
+wait          # wait until programs are finished
---
+++ fractal-plr/img/runs/sub.sh
@@ -0,0 +1,32 @@
+cd broccoli  
+qsub broccoli.sh 
+cd .. 
+
+cd coast  
+qsub coast.sh  
+cd .. 
+
+cd koch2  
+qsub koch2.sh	
+cd .. 
+
+cd koch4  
+qsub koch4.sh  
+cd .. 
+
+cd sierpinski  
+qsub sierpinski.sh  
+cd .. 
+
+cd sierpinski-off  
+qsub sierpinski-off.sh
+cd .. 
+
+cd sphere  
+qsub sphere.sh  
+cd .. 
+
+cd square
+qsub square.sh
+cd .. 
+
