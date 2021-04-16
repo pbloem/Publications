3f49b2b3a0fff950b2eabf4cd1244e617d3b975f
Peter Bloem <git@peterbloem.nl>
2021-04-16 13:00:23 +0200

    ...

---
+++ kgmotifs/results/mutag/mutag.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=72:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+
+module load java/jre-1.8.0
+
+source /home/pbloem/.bashrc
+
+mkdir -p /local/pbloem
+cp /home/pbloem/java/motive-rdf.jar /local/pbloem
+
+cd /home/pbloem/experiments/kgmotifs/mutag
+
+java -jar /local/pbloem/motive-rdf.jar --experiment real-world --iterations 100000 --topk 100 --data mutag --max-time 5
+wait          # wait until programs are finished
+
+rm -rf /local/pbloem
